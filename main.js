1c03b555491b585c689f199f8c18106b3f76bcb6
David Bedo <37118163+bedodavid@users.noreply.github.com>
2018-04-18 11:54:22 +0300

    Add files via upload
    Implemented: 
     - first grade arithmetic
     - show history on top of input

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function () {
     var resultArray = [];
-
-
+    var mathResultInput = 0;
+    var afterOperator = false;
     class InputString {
         constructor(showItems, inputItems) {
             this.showItems = showItems;
@@ -15,8 +15,32 @@ $(function () {
         }
     }
 
-   
 
+    function simpleAritmetic(addendum, operator) {
+        switch (operator) {
+            case "+":
+            {
+                mathResultInput += addendum;
+                break;
+            }
+            case "-":
+            {
+                mathResultInput -= addendum;
+                break;
+            }
+            case "*":
+            {
+                mathResultInput *= addendum;
+                break;
+            }
+            case "/":
+            {
+                mathResultInput /= addendum;
+                break;
+            }
+        }
+        alert(mathResultInput);
+    }
 
     function inputAutofocus() {
         $('#inputString').focus();
@@ -32,13 +56,38 @@ $(function () {
         $('#inputString').val(tmpStr);
     }
 
-    function checkFirtCharNull(inputS){      
-        if (inputS.length === 1 && inputS === "0"){           
+    function checkFirtCharNull(inputS) {
+        if (inputS.length === 1 && inputS === "0") {
             $("#inputString").text("");
             $("#inputString").val("");
         }
     }
-     
+
+    function addToArray(inputValue, showValue) {
+        var inputModel = new InputString(inputValue, showValue);
+        resultArray.push(inputModel);
+    }
+
+    function updateArray(inputValue, showValue, position) {
+        var inputModel = new InputString(inputValue, showValue);
+        resultArray[position] = inputModel;
+    }
+
+    function showHistoryLabel() {
+        var arrayLenght = resultArray.length;
+        var showString = "";
+        for (i = 0; i < arrayLenght - 1; i += 2) {
+            var storedNumber = resultArray[i].getShowItems;
+            if (storedNumber.substring(0, 1) === "-") {
+                storedNumber += "(" + storedNumber + ")";
+            }
+            var storedOperator = resultArray[i + 1].getShowItems;
+            showString = showString + storedNumber + storedOperator;
+        }
+        $("#showInput").text(">" + showString);
+    }
+
+
     function addNumber(e) {
         var inputVal = $("#inputString").val();
         //  alert(inputText+" : "+inputVal);
@@ -48,79 +97,90 @@ $(function () {
         $("#inputString").val(inputVal + currentInput);
     }
 
- 
-   // allowing only a couple of keys to operate
-   $("#inputString").keydown(function (e) {
-        // Allow: backspace, delete,  enter, .   
+
+// allowing only a couple of keys to operate
+    $("#inputString").keydown(function (e) {
+// Allow: backspace, delete,  enter, .   
         if ($.inArray(e.keyCode, [46, 8, 13, 110, 190]) !== -1) {
-            // let it happen, don't do anything
+// let it happen, don't do anything
             return;
         }
-        // Ensure that it is a number and stop the keypress
+// Ensure that it is a number and stop the keypress
         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
             e.preventDefault();
         }
     });
 
 
-
-   // managing events from the calculator screen: number buttons 0-9
+    // managing events from the calculator screen: number buttons 0-9
     $(document).on("click", ".btn-type-1", function () {
         var testStart = $("#inputString").val();
         checkFirtCharNull(testStart);
-        addNumber(this);
+        if (!afterOperator) {
+            addNumber(this);
+        } else {
+            $("#inputString").val("");
+            addNumber(this);
+            afterOperator = false;
+        }
+
     });
-    
-    
     // managing events from the calculator screen: button +-
     $(document).on("click", ".btn-type-7", function () {
-       var testStart = $("#inputString").val();
-       if(testStart!=0){
-        (testStart.substring(0,1)==="-")?$("#inputString").val( testStart.substring(1)):$("#inputString").val( "-"+testStart);   
-       }  
+        var testStart = $("#inputString").val();
+        if (testStart != 0) {
+            (testStart.substring(0, 1) === "-") ? $("#inputString").val(testStart.substring(1)) : $("#inputString").val("-" + testStart);
+        }
     });
-    
     $(document).on("click", ".btn-type-2", function () {
-       var testStart = $("#inputString").val();
-       var inputString=new InputString(testStart, testStart);
-       resultArray.push(inputString);
-       var operatorString= new InputString($(this).text(),$(this).val());
-       resultArray.push(operatorString);
+        var inputString = $("#inputString").val();      
+        
+        if (!afterOperator) {
+            addToArray(inputString, inputString);
+            addToArray($(this).text(), $(this).val());
+            if(resultArray.length>2){
+                var arrayLength=resultArray.length;
+                var addendum = parseInt(resultArray[arrayLength - 2].getInputItems);
+                var operator = resultArray[arrayLength - 3].getInputItems;
+                simpleAritmetic(addendum, operator);
+                afterOperator = true;
+            }else{
+               mathResultInput = parseInt(resultArray[0].getInputItems);  
+            } 
+        } else {
+            updateArray($(this).text(), $(this).val(), resultArray.length - 1);
+            afterOperator = true;
+        }        
+        showHistoryLabel();
+        $("#inputString").text(mathResultInput);
+        $("#inputString").val(mathResultInput);
     });
-    
-    
+
+
     // managing events from the calculator screen: button .
     $(document).on("click", ".btn-type-8", function () {
         var testStart = $("#inputString").val();
-        var decimalPosition=testStart.indexOf(".");        
-           if(decimalPosition<0){
-              $("#inputString").val(testStart+".");
-           }
+        var decimalPosition = testStart.indexOf(".");
+        if (decimalPosition < 0) {
+            $("#inputString").val(testStart + ".");
+        }
     });
-
-
-   //managing event from the keys
+    //managing event from the keys
     $(document).on("keydown", function (e) {
-        var testStart = $("#inputString").val();       
+        var testStart = $("#inputString").val();
         if ((!e.shiftKey && (e.keyCode > 48 && e.keyCode < 57)) || (e.keyCode > 96 && e.keyCode < 105)) {
-           checkFirtCharNull(testStart); 
-           return;
+            checkFirtCharNull(testStart);
+            return;
         }
-        if(e.keyCode===110){
-           var decimalPosition=testStart.indexOf(".");        
-           if(decimalPosition>0){
-              e.preventDefault();
-           }
+        if (e.keyCode === 110) {
+            var decimalPosition = testStart.indexOf(".");
+            if (decimalPosition > 0) {
+                e.preventDefault();
+            }
         }
     });
-
-
-    
-
-
-
     $(document).ready(function () {
-        //inputAutofocus();
+//inputAutofocus();
 
     });
     ;
