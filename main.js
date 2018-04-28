710546379c8cb313fc079f6996f6fa4840e5b85d
David Bedo <37118163+bedodavid@users.noreply.github.com>
2018-04-28 23:44:31 +0300

    Add files via upload
    Implemented: memory buttons,
    Updated: Clear all and Backspace buttons
                    The not simple arithmetic's  buttons

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $(function () {
     var resultArray = [];
     var mathResultInput = 0;
-    ;
     var afterOperator = false;
+    var aftercomplex = "";
+    var inputBeforeComplex = "";
+    var storedMemory = 0;
+
     class InputString {
         constructor(showItems, inputItems) {
             this.showItems = showItems;
@@ -16,10 +19,69 @@ $(function () {
         }
     }
 
+    function secondaryOperations(inputvalue, operator) {
+        var value = BigNumber(inputvalue);
+        var one = BigNumber(1);
+        var result;
+        var inputBeforeComplexLength = inputBeforeComplex.length;
+        if (inputBeforeComplexLength === 0) {
+            inputBeforeComplex = $("#showInput").html();
+            inputBeforeComplexLength = inputBeforeComplex.length;
+        }
 
-    function simpleAritmetic(addendum, operator) {
+        switch (operator) {
+            case "sqrt":
+            {
+                result = value.squareRoot().toPrecision(16);
+                if (aftercomplex === "") {
+                    aftercomplex = "&#8730(" + value + ")";
+                } else {
+                    aftercomplex = "&#8730(" + aftercomplex + ")";
+                }
+
+                break;
+            }
+            case "inv":
+            {
+                result = one.dividedBy(value).toPrecision(16);
+                if (aftercomplex === "") {
+                    aftercomplex = "1/(" + value + ")";
+                } else {
+                    aftercomplex = "1/(" + aftercomplex + ")";
+                }
+                break;
+            }
+            case "pow2":
+            {
+                result = value.times(value).toPrecision(16);
+                if (aftercomplex === "") {
+                    aftercomplex = "(" + value + ")<sup>2</sup>";
+                } else {
+                    aftercomplex = "(" + aftercomplex + ")<sup>2</sup>";
+                }
+                break;
+            }
+            case "pow3":
+            {
+                result = value.times(value).times(value).toPrecision(16);
+                if (aftercomplex === "") {
+                    aftercomplex = "(" + value + ")<sup>3</sup>";
+                } else {
+                    aftercomplex = "(" + aftercomplex + ")<sup>3</sup>";
+                }
+                break;
+            }
+        }
+        $('#inputString').val(result);
+        $('#inputString').text(result);
+        $("#showInput").html(inputBeforeComplex + aftercomplex);
+
+    }
+
+
+    function simpleAritmetic(basicNumber, addendum, operator) {
         var addNumber = BigNumber(addendum);
-        var base = BigNumber(mathResultInput);
+        var base = BigNumber(basicNumber);
         switch (operator) {
             case "+":
             {
@@ -64,7 +126,6 @@ $(function () {
 
     function checkFirtCharNull(inputS) {
         if (inputS.length === 1 && inputS === "0") {
-            alert(inputS.length);
             $("#inputString").text("");
             $("#inputString").val("");
         }
@@ -86,12 +147,12 @@ $(function () {
         for (i = 0; i < arrayLenght - 1; i += 2) {
             var storedNumber = resultArray[i].getShowItems;
             if (storedNumber.substring(0, 1) === "-") {
-                storedNumber += "(" + storedNumber + ")";
+                storedNumber = "(" + storedNumber + ")";
             }
             var storedOperator = resultArray[i + 1].getShowItems;
             showString = showString + storedNumber + storedOperator;
         }
-        $("#showInput").text(showString);
+        $("#showInput").html(showString);
     }
 
 
@@ -121,6 +182,7 @@ $(function () {
 
     // managing events from the calculator screen: number buttons 0-9
     $(document).on("click", ".btn-type-1", function () {
+        inputBeforeComplex = "";
         var testStart = $("#inputString").val();
         checkFirtCharNull(testStart);
         if (!afterOperator) {
@@ -132,26 +194,25 @@ $(function () {
         }
 
     });
-    // managing events from the calculator screen: button +-
-    $(document).on("click", ".btn-type-7", function () {
-        var testStart = $("#inputString").val();
-        if (testStart != 0) {
-            (testStart.substring(0, 1) === "-") ? $("#inputString").val(testStart.substring(1)) : $("#inputString").val("-" + testStart);
-        }
-        afterOperator = false;
-    });
 
     $(document).on("click", ".btn-type-2", function () {
+        inputBeforeComplex = "";
         var inputString = $("#inputString").val();
 
         if (!afterOperator) {
-            addToArray(inputString, inputString);
+            if (aftercomplex === "") {
+                addToArray(inputString, inputString);
+            } else {
+                addToArray(aftercomplex, inputString);
+                aftercomplex = "";
+            }
             addToArray($(this).text(), $(this).val());
+
             if (resultArray.length > 2) {
                 var arrayLength = resultArray.length;
                 var addendum = resultArray[arrayLength - 2].getInputItems;
                 var operator = resultArray[arrayLength - 3].getInputItems;
-                simpleAritmetic(addendum, operator);
+                simpleAritmetic(mathResultInput, addendum, operator);
                 afterOperator = true;
             } else {
                 mathResultInput = resultArray[0].getInputItems;
@@ -167,6 +228,102 @@ $(function () {
     });
 
 
+    //managing the "bit" more complex operations %, 1/x, sqrt, x^2, x^3
+    $(document).on("click", ".btn-type-3", function () {
+        var inputString = $("#inputString").val();
+        secondaryOperations(inputString, $(this).val());
+        afterOperator = false;
+    });
+
+
+
+    // managing events from the calculator screen: button AC
+    $(document).on("click", ".btn-type-4", function () {
+        var operation=$(this).val();
+        if (operation==="clear"){
+         resultArray.length = 0;
+         mathResultInput = 0;
+        $('#inputString').val("");
+        $('#inputString').text("0");
+        $("#showInput").text("");
+        aftercomplex = "";
+        inputBeforeComplex = "";   
+        }else{
+            var inputStr=$('#inputString').val();
+            inputStr=inputStr.substr(0,inputStr.length-1);
+            if(inputStr.length>0){             
+            $('#inputString').val(inputStr);
+            $('#inputString').text(inputStr);   
+            }else{
+             $('#inputString').val("0");
+            $('#inputString').text("0");    
+            }
+           
+           
+           
+        }
+        
+    });
+
+    $(document).on("click", ".btn-type-5", function () {
+        var memoryOperation = $(this).val();
+        switch (memoryOperation) {
+            case "ms":
+            {
+                storedMemory = $('#inputString').text();
+                $("#mr").prop("disabled", false);
+                $("#mc").prop("disabled", false);
+                break;
+            }
+            case "m+":
+            {
+                memValue = BigNumber(storedMemory);
+                simpleAritmetic($('#inputString').val(), memValue, "+");
+                $("#inputString").text(mathResultInput);
+                $("#inputString").val(mathResultInput);
+                aftercomplex = "";
+                inputBeforeComplex = "";
+                break;
+            }
+            case "m-":
+            {
+                memValue = BigNumber(storedMemory);
+                simpleAritmetic($('#inputString').val(), memValue, "-");
+                $("#inputString").text(mathResultInput);
+                $("#inputString").val(mathResultInput);
+                aftercomplex = "";
+                inputBeforeComplex = "";
+                break;
+            }
+            case "mc":
+            {
+                storedMemory = 0;
+                $("#mr").prop("disabled", true);
+                $("#mc").prop("disabled", true);
+                break;
+
+            }
+            case "mr":
+            {
+                $("#inputString").text(storedMemory);
+                $("#inputString").val(storedMemory);
+                break;
+
+            }
+
+        }
+
+    });
+
+    // managing events from the calculator screen: button +-
+    $(document).on("click", ".btn-type-7", function () {
+        var testStart = $("#inputString").val();
+        if (testStart != 0) {
+            (testStart.substring(0, 1) === "-") ? $("#inputString").val(testStart.substring(1)) : $("#inputString").val("-" + testStart);
+        }
+        afterOperator = false;
+    });
+
     // managing events from the calculator screen: button .
     $(document).on("click", ".btn-type-8", function () {
         if (!afterOperator) {
@@ -175,14 +332,15 @@ $(function () {
             if (decimalPosition < 0) {
                 $("#inputString").val(testStart + ".");
             }
-        }else{
-           $("#inputString").val("0.");
-           $("#inputString").text("0."); 
+        } else {
+            $("#inputString").val("0.");
+            $("#inputString").text("0.");
         }
-
 
         afterOperator = false;
     });
+
+
     //managing event from the keys
     $(document).on("keydown", function (e) {
         var testStart = $("#inputString").val();
